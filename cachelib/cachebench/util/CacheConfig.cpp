39ce2cf64dea9cde5fc1e1bf25d80514b817f161
Sara Jane McAllister <sjmcalli@h0.kangaroo-zns.flash-caching.narwhal.pdl.cmu.edu>
2022-11-18 16:59:17 -0500

    Merge with WD's ZNS commits

--- cachelib/cachebench/util/CacheConfig.cpp
+++ cachelib/cachebench/util/CacheConfig.cpp
@@ -104,7 +104,7 @@ CacheConfig::CacheConfig(const folly::dynamic& configJson) {
   // if you added new fields to the configuration, update the JSONSetVal
   // to make them available for the json configs and increment the size
   // below
-  checkCorrectSize<CacheConfig, 720>();
+  checkCorrectSize<CacheConfig, 728>();
 
   if (numPools != poolSizes.size()) {
     throw std::invalid_argument(folly::sformat(
